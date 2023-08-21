6731b890d84fa1e089839a302b428a59d0980f1c
Gabriel Lopes <gabriellopes031997@hotmail.com>
2023-08-21 11:47:41 -0300

    Creating updated implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "grag@gmal.com", new Date(), 40000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id= " + newSeller.getId());
+		
+		System.out.println("\n=== Test 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta");
+		sellerDao.update(seller);
+		System.out.println("Updated Completed");
 	}
 
 }
